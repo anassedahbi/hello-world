d354d01fc81e65ed6069e3d1bb83e3864e93b002
ANAS EDAHBI <edahbianass@gmail.com>
2025-01-22 23:09:48 +0000

    First commit

--- src/main/java/org/formation/HelloWorldController.java
+++ src/main/java/org/formation/HelloWorldController.java
@@ -9,6 +9,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String helloWorld(){
-        return "Hello     dd NAS me hhy ";
+        return "Hello     dd NAS me hhfgfgy ";
     }
 }
